37245d4dee14560ca84c7b510a17c0fa16a1f3a6
Tyler Reardon <tdr.reardon@gmail.com>
2017-01-19 09:34:23 -0800

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>this is the title</title>
+    <link href="/resources/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <script src="./resources/js/header.js"></script>
+    <header>
+      <h1>this is heading</h1>
+      <div><?php echo 1 + 1; ?></div>
+    </header>
+  </body>
+</html>
