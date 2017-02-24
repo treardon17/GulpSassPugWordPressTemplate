7bdccbea1a0e6011b68acf310bf32fa0826a6505
Tyler Reardon <tdr.reardon@gmail.com>
2017-02-24 11:44:44 -0800

    Minor improvements and support for html compiling

--- src/resources/js/main.js
+++
@@ -1,8 +0,0 @@
-function currentYear(){
-    let currentYear = new Date().getFullYear();
-    $('.current-year').text(' ' + currentYear + '. ')
-}
-
-$(document).ready(()=>{
-    currentYear();
-});
--- src/resources/js/parallax.js
+++ src/resources/js/parallax.js
@@ -6,7 +6,7 @@ class ParallaxPage{
         for(let i = 0; i < parallaxContainers.length; i++){
             let parallaxElements = $(parallaxContainers[i]).find('.parallax-element');
             let titleHeadings = $(parallaxContainers[i]).find('.title-heading');
-            this.parallaxContainers.push({container: parallaxContainers[i], elements: parallaxElements, titleHeadings: titleHeadings})
+            this.parallaxContainers.push({container: parallaxContainers[i], elements: parallaxElements, titleHeadings: titleHeadings, position: $(parallaxContainers[i]).offset().top});
         }
         //set initial position of elements
         this.handleScroll();
@@ -22,19 +22,17 @@ class ParallaxPage{
 
             //check if the element we're observing is in the current viewport
             this.checkElementInView(container,{
-                //if the top of the window is in the middle of the element we're observing,
-                //move the elements and change their opacity
-                middleCallback: (percentComplete)=>{
-                    this.moveElements(elements, -percentComplete*250);
-                    this.opacityElements(elements, 1-percentComplete);
+                elementInViewCallback: (percentComplete)=>{
+                    let distanceToTravel = $(container).outerHeight() * 0.33;
+                    this.moveElements(elements, percentComplete * distanceToTravel);
                 }
-            })
+            });
         }
     }
 
     moveElements(elements, margin){
         for(let i = 0; i < elements.length; i++){
-            $(elements[i]).css({'bottom': margin + 'px'});
+            $(elements[i]).css({'top': margin + 'px'});
         }
     }
 
@@ -46,12 +44,20 @@ class ParallaxPage{
 
     //Checks to see if an element is above, within, or below the current scroll offset
     checkElementInView(elementSelector, cbObject){
-        var top  = window.pageYOffset || document.documentElement.scrollTop;    //top of the window
-        var bottom = top + $(window).height();                                  //bottom of the window
+        const top  = window.pageYOffset || document.documentElement.scrollTop;    //top of the window
+        const windowHeight = $(window).height();
+        const bottom = top + windowHeight;                                        //bottom of the window
+        
 
-        var element = $(elementSelector);       //div being observed
-        var height = $(element).outerHeight();  //height of the element
-        var offset = $(element).offset().top;   //the top of the element
+        const element = $(elementSelector);       //div being observed
+        const height = $(element).outerHeight();  //height of the element
+        const offset = $(element).offset().top;   //the top of the element
+
+        const bottomInView = (bottom >= offset && bottom < offset + height);
+        const topInView = (top >= offset && top < offset + height);
+        const aboveView = (top - offset < height);
+        const belowView = (top > offset + height);
+        const viewInViewport = (bottom > offset || top < offset + height);
 
         //this will prevent the new top from being set 
         if(top % 2 == 0){
@@ -65,29 +71,35 @@ class ParallaxPage{
             this.previousTop = top;
         }
 
-        if(top >= offset && top < offset + height){
+        if(topInView){
             //if the top of the viewport is in the middle of the element we're observing
             if(typeof cbObject.middleCallback === 'function'){
                 let percentComplete = (top - offset)/height;
                 cbObject.middleCallback(percentComplete);
             }
-        }else if(top - offset < height){
+        }else if(aboveView){
             //if the viewport is above the view we're observing
             if(typeof cbObject.aboveCallback === 'function'){
                 cbObject.aboveCallback();
             }
-        }else if (top > offset + height){
+        }else if (belowView){
             //if the viewport is below the view we're observing
             if(typeof cbObject.belowCallback === 'function'){
                 cbObject.belowCallback();
             }
-        }
-
-        //if the bottom of the window is in the middle of the element we're observing
-        if(bottom >= offset && bottom < offset+height){
+        }else if(bottomInView){
+            //if the bottom of the window is in the middle of the element we're observing
             if(typeof cbObject.bottomProgressCallback === 'function'){
                 let percentComplete = (bottom - offset)/height
                 cbObject.bottomProgressCallback(percentComplete);
+            }
+        }
+
+        //if any part of the element is in the view
+        if(topInView || viewInViewport || bottomInView){
+            if(typeof cbObject.elementInViewCallback === 'function'){
+                let percentComplete = (top - offset)/((2*height) + windowHeight);
+                cbObject.elementInViewCallback(percentComplete);
             }
         }
     }
